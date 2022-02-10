5134faa928a771fa2d04e4205de1d766d74928aa
leonflamant <leon.flamant@hotmail.fr>
2022-02-10 12:48:17 +0100

    launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Launcher {
-
-
-
-}
+    public static void main(String[] args) {
+        System.out.println("bienvenu");
+        Scanner scan = new Scanner(System.in);
+        String commande = scan.nextLine();
+        if (!commande.equals("quit")) {
+            System.out.println("unknown commande");
+        }
+}}
